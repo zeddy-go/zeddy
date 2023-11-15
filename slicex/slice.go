ba2768e63d55a5bda3ab036601596719dbc9267b
zed <zedsmail@163.com>
2023-11-15 18:53:01 +0800

    --

--- slicex/slice.go
+++ slicex/slice.go
@@ -2,6 +2,52 @@ package slicex
 
 import "errors"
 
+func Find[T any](f func(item T) bool, arr ...T) (result T, ok bool) {
+	if len(arr) == 0 {
+		return
+	}
+
+	for _, item := range arr {
+		if f(item) {
+			ok = true
+			result = item
+			return
+		}
+	}
+
+	return
+}
+
+func Filter[T any](f func(item T) bool, arr ...T) (list []T) {
+	if len(arr) == 0 {
+		return
+	}
+
+	list = make([]T, 0, len(arr))
+	for _, item := range arr {
+		if !f(item) {
+			list = append(list, item)
+		}
+	}
+
+	return
+}
+
+func Last[T any](arr ...T) (result T) {
+	if len(arr) == 0 {
+		return
+	}
+
+	return arr[len(arr)-1]
+}
+
+func Pop[T any](arr ...T) (newArr []T, result T) {
+	if len(arr) == 0 {
+		return
+	}
+	return arr[:len(arr)-1], arr[len(arr)-1]
+}
+
 func First[T any](arr ...T) (result T) {
 	if len(arr) == 0 {
 		return
