8f54a70fdd8b0e38ca7cb2acce863ef70dd13255
zed <zedsmail@163.com>
2023-11-16 11:40:40 +0800

    --

--- contract/module.go
+++ contract/module.go
@@ -1,9 +1,19 @@
 package contract
 
-import "github.com/gin-gonic/gin"
+type IRouter interface {
+	Any(route string, handler any, middlewares ...any) IRouter
+	GET(route string, handler any, middlewares ...any) IRouter
+	POST(route string, handler any, middlewares ...any) IRouter
+	DELETE(route string, handler any, middlewares ...any) IRouter
+	PATCH(route string, handler any, middlewares ...any) IRouter
+	PUT(route string, handler any, middlewares ...any) IRouter
+	OPTIONS(route string, handler any, middlewares ...any) IRouter
+	HEAD(route string, handler any, middlewares ...any) IRouter
+	Group(prefix string, middlewares ...any) IRouter
+}
 
 type IShouldRegisterRoute interface {
-	RegisterRoute(r gin.IRouter)
+	RegisterRoute(r IRouter)
 }
 
 type IModule interface {
